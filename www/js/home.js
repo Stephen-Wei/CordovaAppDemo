77d4172ba5f8609850f917bc9d5dc3445bc112e4
StephenWei <stephen62622@gmail.com>
2016-10-23 12:53:59 +0800

    html revised

---
+++ www/js/home.js
@@ -0,0 +1,89 @@
+/**
+ * You must include the dependency on 'ngMaterial' 
+ */
+angular.module('MyApp', ['ngMaterial'])
+.controller('AppCtrl', function($scope) {
+	var imagePath = '../img/logo.png';
+	$scope.todos = [
+      {
+        face : imagePath,
+        what: 'javaShow',
+        who: 'javaShow',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'javaShowJs',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'jsShowJava',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'jsShowJs',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'openVideo',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'openActivity',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'batteryStatus',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+    ];
+
+});
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+    	clickItem(index)
+        console.log('Received Event: ' + id);
+    },
+
+};
+
+app.initialize();
+
+
+function clickItem(index) {
+	// body...
+}
