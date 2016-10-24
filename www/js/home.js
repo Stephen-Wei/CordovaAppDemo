326ac8713d8e2d9af1094258617bd254043a6874
StephenWei <stephen62622@gmail.com>
2016-10-24 19:42:50 +0800

    html page revised

--- www/js/home.js
+++ www/js/home.js
@@ -56,34 +56,66 @@ angular.module('MyApp', ['ngMaterial'])
       },
     ];
 
+	$scope.app = {
+	    // Application Constructor
+	    initialize: function() {
+	        this.bindEvents();
+	    },
+	    // Bind Event Listeners
+	    //
+	    // Bind any events that are required on startup. Common events are:
+	    // 'load', 'deviceready', 'offline', and 'online'.
+	    bindEvents: function() {
+	        document.addEventListener('deviceready', this.onDeviceReady, false);
+	    },
+	    // deviceready Event Handler
+	    //
+	    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+	    // function, we must explicitly call 'app.receivedEvent(...);'
+	    onDeviceReady: function() {
+          console.log("---->ready!");
+	        ready();
+	    },
+	};
+
+	$scope.app.initialize();
+
+	function ready() {
+      $scope.click=function(index){
+          console.log("---->index:"+ index);
+          switch(index){
+            case 0:
+              console.log("---->todo: javaShow");
+
+              break;
+            case 1:
+              console.log("---->todo: javaShowJs");
+
+              break;
+            case 2:
+              console.log("---->todo: jsShowJava");
+
+              break;
+            case 3:
+              console.log("---->todo: jsShowJs");
+
+              break;
+            case 4:
+              console.log("---->todo: openVideo");
+
+              break;
+            case 5:
+              console.log("---->todo: openActivity");
+
+              break;
+            case 6:
+              console.log("---->todo: batteryStatus");
+
+              break;
+          }
+      };
+	}
+
 });
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-    	clickItem(index)
-        console.log('Received Event: ' + id);
-    },
 
-};
-
-app.initialize();
-
-
-function clickItem(index) {
-	// body...
-}
